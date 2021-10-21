ea0fb2742e206783826b803c095c0e11d6886abf
BerendK <90273647+BerendK@users.noreply.github.com>
2021-10-21 21:01:02 +0200

    ok

--- CSD2a/list genrerator.py
+++ CSD2a/list genrerator.py
@@ -9,45 +9,106 @@ timestamp1=[]
 timestamp2=[]
 timestamp3=[]
 
-def genratetimestamps(bpm,maat1,maat2,maat3):
-    for i in range(0,maat1):
-        timestamp1.append()
 
-def genratelists(amount,maat1,maat2,maat3):
+
+
+
+
+# def genratelists(amount,maat1,maat2,maat3)   
+#     for i in range(0,maat1):
+#         thing = random.random()*amount
+#         randval1.append(thing)
+#     for i in range(0,maat2):
+#         thing = random.random()*amount
+#         randval2.append(thing)
+#     for i in range(0, maat3):
+#         thing= random.random()*amount
+#         randval3.append(thing)
+
+
+
+
+bpm = int(input('BPM: '))
+if bpm < 0 :
+    bpm = bpm* (-1)
+interval16 = 60/bpm
+lengthall=interval16*16
+print(interval16)
+print(lengthall)
+
+# loopamount=int(input('hoevaak loopen?: '))
+# if loopamount < 0 :
+#     loopamount = loopamount* (-1)
+# print(loopamount)
+
+
+kickgrid=int(input('Kick grid: '))
+if kickgrid < 0 :
+    kickgrid = kickgrid* (-1)
+snaregrid=int(input('Snare grid: '))
+if snaregrid < 0 :
+    snaregrid = snaregrid* (-1)
+hatgrid=int(input('Hat grid: '))
+if snaregrid < 0 :
+    snaregrid = snaregrid* (-1)
+
+
+
+
+for i in range(0,kickgrid):
+    kickinterval = lengthall/kickgrid
+    timestamp1.append(kickinterval*i)
+    randomness=(random.random()/(((i) % (kickgrid/4))+1))
+    if  randomness< 0.4:
+
+        randval1.append(0)
+    else:
+        randval1.append(1)
+
+
+for i in range(0,snaregrid):
+    snareinterval = lengthall/snaregrid
+    timestamp2.append(snareinterval*i)
+    randomness=(random.random()/(((i) % (snaregrid/2))+1))
+    if  randomness< 0.2:
+
+        randval2.append(0)
+    else:
+        randval2.append(1)
+
+
+for i in range(0,hatgrid):
+    hatinterval = lengthall/hatgrid
+    timestamp3.append(hatinterval*i)
+    randomness=(random.random()/(((i) % (hatgrid/8))+1))
+    if  randomness< 0.2:
+
+        randval3.append(0)
+    else:
+        randval3.append(1)
     
-    for i in range(0,maat1):
-        thing = random.random()*amount
-        randval1.append(thing)
-    for i in range(0,maat2):
-        thing = random.random()*amount
-        randval2.append(thing)
-    for i in range(0, maat3):
-        thing= random.random()*amount
-        randval3.append(thing)
 
 
 
 
-bpm = input('BPM: ')
-bpm=int(bpm)
 
-
-
-
-genratelists(1,int(input('maat1: ')),int(input('maat2: ')),int(input('maat3: ')))
+# genratelists(1,int(input('maat1: ')),int(input('maat2: ')),int(input('maat3: ')))
 
 print("\n")
 print(randval1)
+print(timestamp1)
 print("\n")
 print(randval2)
+print(timestamp2)
 print("\n")
 print(randval3)
+print(timestamp3)
 print("\n")
-loopsave=input('loop=l save=s: ')
-if loopsave=='l':
-    print('loop')
-elif loopsave=='s':
-    print('save')
-else:
-    print('invalid response')
+# loopsave=input('loop=l save=s: ')
+# if loopsave=='l':
+#     print('loop')
+# elif loopsave=='s':
+#     print('saved')
+# else:
+#     print('invalid response')
 
--- CSD2a/randomtest.py
+++ CSD2a/randomtest.py
@@ -15,52 +15,125 @@ wave_read = wave.open('CSD2a/Hat.wav', 'rb')
 hat = sa.WaveObject.from_wave_read(wave_read)
 
 
-timestamp1=[1,2,3,4,5,6,7,8,9,10]
-timestamp2=[2,4,6,8,10]
-timestamp3=[0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
+timestamp1=[]
+timestamp2=[]
+timestamp3=[]
+randval1=[]
+randval2=[]
+randval3=[]
 
-tijdtoen=time.time()+timestamp1[0]
-kick.play()
+
+
+bpm = int(input('BPM: '))
+if bpm < 0 :
+    bpm = bpm* (-1)
+interval16 = 60/bpm
+lengthall=interval16*16
+print(interval16)
+print(lengthall)
+
+kickgrid=int(input('Kick grid: '))
+if kickgrid < 0 :
+    kickgrid = kickgrid* (-1)
+snaregrid=int(input('Snare grid: '))
+if snaregrid < 0 :
+    snaregrid = snaregrid* (-1)
+hatgrid=int(input('Hat grid: '))
+if snaregrid < 0 :
+    snaregrid = snaregrid* (-1)
+
+
+
+for i in range(0,kickgrid):
+    kickinterval = lengthall/kickgrid
+    timestamp1.append(kickinterval*i)
+    randomness=(random.random()/(((i) % (kickgrid/4))+1))
+    if  randomness< 0.3:
+
+        randval1.append(0)
+    else:
+        randval1.append(1)
+
+
+for i in range(0,snaregrid):
+    snareinterval = lengthall/snaregrid
+    timestamp2.append(snareinterval*i)
+    randomness=(random.random()/(((i) % (snaregrid/2))+1))
+    if  randomness< 0.2:
+
+        randval2.append(0)
+    else:
+        randval2.append(1)
+
+
+for i in range(0,hatgrid):
+    hatinterval = lengthall/hatgrid
+    timestamp3.append(hatinterval*i)
+    randomness=(random.random())
+    if  randomness< 0.4:
+
+        randval3.append(0)
+    else:
+        randval3.append(1)
+
+
+
+
+tijdtoen=time.time()
 
 i1=0
 i2=0
 i3=0
-count=0
-tijdtoen2 = time.time()
+
+
 kickdone = 0
 snaredone = 0
 hatdone = 0
-print('lengte',len(timestamp2))
-while True:
-    #test counter
-    # if (time.time()-tijdtoen2) >= 1:
-    #     tijdtoen2 = time.time()
-    #     count = count + 1
-    #     print(count)
-        
+loopcount = 0
 
+print('lengte',len(timestamp2))
+
+print("\n")
+for i in range(0,len(randval1)):
+    print(randval1[i])
+print(randval1)
+print(timestamp1)
+print("\n")
+print(randval2)
+print(timestamp2)
+print("\n")
+print(randval3)
+print(timestamp3)
+print("\n")
+
+
+
+while True:
 
     tijdnu = time.time()
 
  
 
     # kick
-    if (tijdnu - tijdtoen) >= timestamp1[i1] and (kickdone==0) :
-        print('kick',i1,(tijdnu - tijdtoen),kickdone)
-        kick.play()
+    if (tijdnu - tijdtoen) >= timestamp1[i1] and (kickdone==0):
+
+        if randval1[i1]==1:
+            print('kick',i1)
+            kick.play()
         
         if i1 == (len(timestamp1)-1):
             kickdone = 1
             print('nice')
-            
-
         else:
             i1 = i1 + 1
+            
     
     # snare
     if (tijdnu - tijdtoen) >= timestamp2[i2] and (snaredone==0) :
-        print('snare',i2,(tijdnu - tijdtoen),snaredone)
-        snare.play()
+        
+        if randval2[i2]==1:
+            print('snare',i2)
+            snare.play()
         
         if i2 == (len(timestamp2)-1):
             snaredone = 1
@@ -72,8 +145,10 @@ while True:
 
     # hat
     if (tijdnu - tijdtoen) >= timestamp3[i3] and (hatdone==0) :
-        print('hat',i3,(tijdnu - tijdtoen),hatdone)
-        hat.play()
+        
+        if randval3[i3]==1:
+            print('hat',i3)
+            hat.play()
         
         if i3 == (len(timestamp3)-1):
             hatdone = 1
@@ -84,14 +159,7 @@ while True:
             i3 = i3 + 1
 
 
-
-
-        
-
-
-
-    
-    if (kickdone==1)and(snaredone==1):
+    if (tijdnu - tijdtoen) >= lengthall:
         tijdtoen=tijdnu
         print('zeerlekker')
         kickdone=0
@@ -100,5 +168,18 @@ while True:
         i1=0
         i2=0
         i3=0
+        loopcount += 1
+        print('loopcount= ',loopcount)
+        if loopcount>1:
+            loopcount=0
+            input("Loop=l Save=s")
+            tijdtoen=time.time()
+        
 
-    time.sleep(0.01)
+
+
+    time.sleep(0.001)
+
+print('epic')
+
+time.sleep(1)
